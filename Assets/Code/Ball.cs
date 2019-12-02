3d33a2773583e2f9f55c7460c6022fc35d006c50
brndnwrd <brndnwrd@gmail.com>
2019-12-01 20:28:07 -0600

    initial commit of game

---
+++ Assets/Code/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public AudioClip PointScored;
+    public AudioClip GameOverSound;
+
+    internal void Start()
+    {
+
+    }
+
+    internal void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().position = new Vector2(2.225f, 4f);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            ScoreKeeper.Reset();
+        }
+    }
+
+    internal void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Target":
+                GetComponent<Rigidbody2D>().position = new Vector2(2.225f, 4f);
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                ScoreKeeper.AddToScore(1f);
+                GetComponent<AudioSource>().PlayOneShot(PointScored);
+                break;
+            case "Drain":
+                ScoreKeeper.GameOver();
+                GetComponent<AudioSource>().PlayOneShot(GameOverSound);
+                break;
+        }
+    }
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Transform>().GetChild(0).GetComponent<Rigidbody2D>().centerOfMass = new Vector3(0, 0, 0);
+        GetComponent<Transform>().GetChild(1).GetComponent<Rigidbody2D>().centerOfMass = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Hit right flipper
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Transform>().GetChild(0).GetComponent<Rigidbody2D>().MoveRotation(-60f);
+        }
+        else
+        {
+            GetComponent<Transform>().GetChild(0).GetComponent<Rigidbody2D>().MoveRotation(10f);
+        }
+
+        // Hit left flipper
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Transform>().GetChild(1).GetComponent<Rigidbody2D>().MoveRotation(240f);
+        }
+        else
+        {
+            GetComponent<Transform>().GetChild(1).GetComponent<Rigidbody2D>().MoveRotation(170f);
+        }
+    }
+}
